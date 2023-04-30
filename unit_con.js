2e1a1a85d9712a87791c7ca657d9f006e44e1815
Frozen-fire2001 <127422834+Frozen-fire2001@users.noreply.github.com>
2023-04-30 15:52:16 +0530

    Update unit_con.js

--- unit_con.js
+++ unit_con.js
@@ -4,22 +4,22 @@ var unit = new Array();
 var factor = new Array();
 
 property[0] = "Length";
-unit[0] = new Array("Meter (m)", "Angstrom (A')", "Astronomical unit (AU)", "Caliber (cal)", "Centimeter (cm)", "Kilometer (km)", "Ell", "Em", "Fathom", "Furlong", "Fermi (fm)", "Foot (ft)", "Inch (in)", "League (int'l)", "League (UK)", "Light year (LY)", "Micrometer (mu-m)", "Mil", "Millimeter (mm)", "Nanometer (nm)", "Mile (int'l nautical)", "Mile (UK nautical)", "Mile (US nautical)", "Mile (US statute)", "Parsec", "Pica (printer)", "Picometer (pm)", "Point (pt)", "Rod", "Yard (yd)");
-factor[0] = new Array(1, 1E-10, 1.49598E11, .000254, .01, 1000, 1.143, 4.2323E-03, 1.8288, 201.168, 1E-15, .3048, .0254, 5556, 5556, 9.46055E+15, .000001, .0000254, .001, 1E-9, 1852, 1853.184, 1852, 1609.344, 3.08374E+16, 4.217518E-03, 1E-12, .0003514598, 5.0292, .9144);
+unit[0] = new Array("Meter (m)", "Astronomical unit (AU)",  "Centimeter (cm)", "Kilometer (km)", "Foot (ft)", "Inch (in)", "League (int'l)", "League (UK)", "Light year (LY)", "Micrometer (mu-m)", "Mil", "Millimeter (mm)", "Nanometer (nm)", "Mile (int'l nautical)", "Parsec", "Pica (printer)", "Picometer (pm)", "Point (pt)", "Rod", "Yard (yd)");
+factor[0] = new Array(1, 1.49598E11, .01, 1000,.3048, .0254, 5556, 5556, 9.46055E+15, .000001, .0000254, .001, 1E-9, 1852, 3.08374E+16, 4.217518E-03, 1E-12, .0003514598, 5.0292, .9144);
 
 property[1] = "Mass";
-unit[1] = new Array("Kilogram (kgr)", "Gram (gr)", "Milligram (mgr)", "Microgram (mu-gr)", "Carat (metric)(ct)", "Hundredweight (long)", "Hundredweight (short)", "Pound mass (lbm)", "Pound mass (troy)", "Ounce mass (ozm)", "Ounce mass (troy)", "Slug", "Ton (assay)", "Ton (long)", "Ton (short)", "Ton (metric)", "Tonne");
-factor[1] = new Array(1, .001, 1e-6, .000000001, .0002, 50.80235, 45.35924, .4535924, .3732417, .02834952, .03110348, 14.5939, .02916667, 1016.047, 907.1847, 1000, 1000);
+unit[1] = new Array("Kilogram (kgr)", "Gram (gr)", "Milligram (mgr)", "Microgram (mu-gr)", "Carat (metric)(ct)", "Hundredweight ", "Pound mass ", "Ounce mass", "Slug", "Ton (assay)", "Ton (long)", "Ton (short)", "Ton (metric)", "Tonne");
+factor[1] = new Array(1, .001, 1e-6, .000000001, .0002, 45.35924, .3732417, .03110348,1000);
 
 
 property[2] = "Temperature";
-unit[2] = new Array("Degrees Celsius ('C)", "Degrees Fahrenheit ('F)", "Degrees Kelvin ('K)", "Degrees Rankine ('R)");
-factor[2] = new Array(1, 0.555555555555, 1, 0.555555555555);
-tempIncrement = new Array(0, -32, -273.15, -491.67);
+unit[2] = new Array("Degrees Celsius ('C)", "Degrees Fahrenheit ('F)", "Degrees Kelvin ('K)");
+factor[2] = new Array(1, 0.555555555555, 1);
+tempIncrement = new Array(0, -32, -273.15);
 
 property[3] = "Time";
-unit[3] = new Array("Second (sec)", "Day (mean solar)", "Day (sidereal)", "Hour (mean solar)", "Hour (sidereal)", "Minute (mean solar)", "Minute (sidereal)", "Month (mean calendar)", "Second (sidereal)", "Year (calendar)", "Year (tropical)", "Year (sidereal)");
-factor[3] = new Array(1, 8.640E4, 86164.09, 3600, 3590.17, 60, 60, 2628000, .9972696, 31536000, 31556930, 31558150);
+unit[3] = new Array("Second (sec)", "Hour ", "Minute ", "Month ", "Year (calendar)");
+factor[3] = new Array(1, 3600, 60, 2628000, 31536000);
 
 
 function UpdateUnitMenu(propMenu, unitMenu) {
